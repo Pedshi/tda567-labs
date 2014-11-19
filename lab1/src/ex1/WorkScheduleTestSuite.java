5ea92c4824b0a748523b10499e70745e2e536082
Touchebag <someone90_6@hotmail.com>
2014-11-19 11:15:34 +0100

    Added main tast suite files

---
+++ lab1/src/ex1/WorkScheduleTestSuite.java
@@ -0,0 +1,14 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+  SetRequiredNumberTest.class,
+  AddWorkingPeriodTest.class,
+  WorkingEmployeesTest.class,
+  NextIncompleteTest.class
+})
+
+public class WorkScheduleTestSuite {
+    // nothing goes here
+}
---
+++ lab1/src/ex2/SetTestSuite.java
@@ -0,0 +1,14 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+  SetInsertTest.class,
+  SetMemberTest.class,
+  SetSectionTest.class,
+  SetContainsArithTripleTest.class
+})
+
+public class SetTestSuite {
+// nothing goes here
+}
